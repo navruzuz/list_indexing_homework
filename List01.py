fd988fd4de07df37117a05f10095e65b6b1eb12c
navruzuz <hamrayevnavruzbek9906@gmail.com>
2023-07-26 17:44:47 +0500

    bajarildi

--- List01.py
+++ List01.py
@@ -1,9 +1,10 @@
-def main():
-    """
-    Return an empty list to the result.
-    Args:
-        None
-    Returns:
-        list: return answer
-    """
-    return
+# def main():
+    # """
+    # Return an empty list to the result.
+    # Args:
+    #     None
+    # Returns:
+    #     list: return answer
+    # """
+s=['python',4,5]
+print(s[len(s):1])
